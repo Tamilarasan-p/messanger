a3ac6807d088374236b8389885a1d06a220c99ac
Tamilarasan-p <tamilarasan.p.91@gmail.com>
2021-03-12 16:23:31 +0530

    Webhook changes

--- src/controllers/facebookController.js
+++ src/controllers/facebookController.js
@@ -111,7 +111,7 @@ function callSendAPI(sender_psid, response) {
       "json": body
   }, (err, res, body) => {
       if (!err) {
-          console.log('message sent!' +JSON.parse(body));
+          console.log('message sent!'+body.message);
       } else {
           console.error("Unable to send message:" + err);
       }
