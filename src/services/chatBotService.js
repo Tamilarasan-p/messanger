a8abec93eb126f5a4e7acbe6baa4550a6012a429
Tamilarasan-p <tamilarasan.p.91@gmail.com>
2021-03-24 14:50:38 +0530

    chatbot service implements

--- src/services/chatBotService.js
+++ src/services/chatBotService.js
@@ -5,7 +5,7 @@ const request= require('request');
 
 let getFacebookUserName=(sender_id)=>{
 
-    return new Promise(  (resolve,reject)=>{
+    return new Promise((resolve,reject)=>{
 
     let URL=`https://graph.facebook.com/${sender_id}?fields=first_name,last_name,profile_pic&access_token=${FB_PAGE_ACCESS_TOKEN}`
     request({
@@ -13,22 +13,21 @@ let getFacebookUserName=(sender_id)=>{
       "method": "GET",
     }, (err, res, body) => {
       if (!err) {
-          let body=JSON.parse(body);
-          let username=`${body.first_name} ${body.last_name}`
+            let body=JSON.parse(body);
+            let username=`${body.first_name} ${body.last_name}`
             resolve(username);
-
-      } else {
+      } 
+      else {
         reject("Unable to send message:" + err);
       }
-    }); 
 
-}
+    }); 
+});
+
+};
 
 
 
 module.exports={
     getFacebookUserName:getFacebookUserName
 }
-
-    
-  }
