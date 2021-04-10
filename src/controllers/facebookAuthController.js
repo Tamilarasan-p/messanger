c2c7692c52b2148fb76160bf2925f69c8955240b
Tamilarasan-p <tamilarasan.p.91@gmail.com>
2021-04-10 11:24:06 +0530

    scopes added

--- src/controllers/facebookAuthController.js
+++ src/controllers/facebookAuthController.js
@@ -29,9 +29,9 @@ passport.serializeUser(function (user, cb) {
             userAccount.photo = profile.photos[0].value;  
             userAccount.email = profile.emails[0].value; 
             userAccount.token = accessToken; 
-            //userAccount.page_name = profile._json.accounts.data[0].name;
-            //userAccount.page_id = profile._json.accounts.data[0].id;
-            //userAccount.page_access_token = profile._json.accounts.data[0].access_token;   
+            userAccount.page_name = profile._json.accounts.data[0].name;
+            userAccount.page_id = profile._json.accounts.data[0].id;
+            userAccount.page_access_token = profile._json.accounts.data[0].access_token;   
             console.log(userAccount);
             return done(null, userAccount);
         });
